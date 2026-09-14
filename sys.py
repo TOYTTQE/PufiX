ad5651e8bb87352cc549944a47b10f39766a4ae7
TOYTTQE <profffanlider@gmail.com>
2026-09-14 13:11:01 +0500

    Edit sys.py

--- sys.py
+++ sys.py
@@ -1,9 +1,7 @@
-COMMANDS = ["date", "time", "version"]
+COMMANDS = ["date", "time"]
 DESCRIPTION = "System information"
 
 import datetime
-
-VERSION = "1.0.0"
 
 def handle(command):
 	if command == "date":
@@ -13,7 +11,3 @@ def handle(command):
 	elif command == "time":
 		now = datetime.datetime.now()
 		print("\033[33m" + now.strftime("%H:%M:%S") + "\033[0m")
-
-	elif command == "version":
-		print("\033[1mPufiX \033[36;1mv" + VERSION + "\033[0m")
-		print("\033[90mInspired by TERMUX ⟩_\033[0m")
